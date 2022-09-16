7edbb125b1c82bc7a99be083fe88fbb9a150329f
Filipe Santana de Oliveira <85876429+undefined@users.noreply.github.com>
2022-09-16 19:21:14 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+    visivel = true; //boolean
+
+    nome = "Filipe Smith?"; //string
+    idade = 16; //number
+    email = 'f@f.com'; //string
+    cpf = "00011122233"; //string
+        
+    nomePet = "Caramelo";
+    raca = "Vira Lata";
+    peso = 35;
+    idadePet = 3;
+
+    onClick(){
+        this.visivel = !this.visivel;
+        this.nome = "Anderson";
+        this.idade = 19;
+    }  
+}
